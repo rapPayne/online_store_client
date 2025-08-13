90073eab5da14c9de52e9574b77229b4a7bb8fe7
Rap Payne <bob@creator.net>
2025-08-13 17:14:03 -0400

    Add Storybook with stories and tests for components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true, "vitest/globals": true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:vitest/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:vitest/recommended', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', 'vitest'],
